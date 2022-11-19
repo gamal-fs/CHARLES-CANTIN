4adbbc9b2d7d51ccaba1ad51dc5a851617a74dfe
Eric <lemoine.e@gmail.com>
2022-11-19 13:12:31 +0100

    First Commit

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+
+  let filters = document.querySelectorAll(".filtres li");
+
+
+  for(let filter of filters) {
+    filter.addEventListener("click",function () {
+      let tag = this.id;
+
+      let images = document.querySelectorAll(".gallery img");
+
+      for(let image of images) {
+        image.classList.replace("active", "inactive");
+
+        if(tag in image.dataset || tag === "*") {
+          image.classList.replace("inactive", "active")
+        }
+      }
+
+    });
+  };
+
+}
